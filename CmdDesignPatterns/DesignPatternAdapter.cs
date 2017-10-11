567413c7b40fba5e3a09282c867218f1288ae135
sorryb <sorryb@hotmail.com>
2017-10-11 17:46:51 +0300

    Add files
    Add files

---
+++ CmdDesignPatterns/DesignPatternAdapter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Test
+{
+    /// <summary>
+    /// The Adapter Design Pattern
+    ///The goal of the Adapter design pattern is to convert one interface to another.
+    /// </summary>
+    public class DesignPatternAdapter
+    {
+
+        public static void XXX_Main()
+        {
+            //var prov = new StwipeProvider("Amazon");
+            //MakeAPayment(prov);//first place to make a change in code
+            //prov.Pay("wetWET","12.03.2001",545454);
+
+            MakeAPayment(new PaySalAdapter()); // only one place to make a change in code
+        }
+
+        private static void MakeAPayment(IPaymentProcessorAdapter provider)
+        {
+            provider.ProcessProviderPayType("Emag", "BRDNOIUOI65656566", "12.03.2155", 213424);
+        }
+
+        private static void MakeAPayment(StwipeProvider provider)//second change
+        {
+            provider.Pay("WERWERWER", "23", 200);//third change
+        }
+    }
+
+    public class StwipeProvider
+    {
+        public StwipeProvider(string merchantKey)
+        {
+
+        }
+
+        // returns false if payment is rejected
+        public bool Pay(string cardNumber, string expiration, decimal amount)
+        {
+            if (amount < 0)
+                return false;
+
+            Console.WriteLine("Pay something");
+            return true;
+        }
+    }
+
+    public class PaySalProvider
+    {
+        // throws exception if payment is rejected
+        public void ProcessPayment(string merchantId, CreditCardDetails cardInfo, decimal amount)
+        {
+            if (amount < 0)
+                throw new Exception("The amount value must be positive !");
+
+            Console.WriteLine("Process payment");
+
+
+
+        }
+    }
+
+    public class CreditCardDetails
+    {
+
+        public CreditCardDetails(string cardNumber, string expiration)
+        {
+
+        }
+    }
+
+    public interface IPaymentProcessorAdapter
+    {
+        // returns false if payment is rejected
+        bool ProcessProviderPayType(string merchantId, string cardNumber, string expiration,
+                            decimal amount);
+    }
+
+    public class StwipeAdapter : IPaymentProcessorAdapter
+    {
+        public bool ProcessProviderPayType(string merchantId, string cardNumber, string expiration,
+                            decimal amount)
+        {
+            var provider = new StwipeProvider(merchantId);
+            return provider.Pay(cardNumber, expiration, amount);
+        }
+    }
+
+    public class PaySalAdapter : IPaymentProcessorAdapter
+    {
+        public bool ProcessProviderPayType(string merchantId,
+            string cardNumber, string expiration,
+            decimal amount)
+            {
+                var provider = new PaySalProvider();
+                try
+                {
+                    var cardInfo = new CreditCardDetails(cardNumber, expiration);
+                    provider.ProcessPayment(merchantId, cardInfo, amount);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+    }
+
+}
---
+++ CmdDesignPatterns/DesignPatternStrategy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace StrategyDesignPattern
+{
+    /// <summary>
+    /// The Strategy design pattern allows an object to have some of the details of its behavior encapsulated in another type, which is then provided to it as a parameter.
+    /// This pattern is closely related to dependency injection, which refers to the technique of passing dependencies (or injecting them) into classes, usually through their constructor.
+    /// Strategy Design Pattern.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void XXXMain()
+        {
+            Context context;
+
+            // Three contexts following different strategies
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("A. Called ConcreteStrategyA.AlgorithmInterface() but in Context Class.");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("B. Called ConcreteStrategyB.AlgorithmInterface() but in Context Class.");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("C. Called ConcreteStrategyC.AlgorithmInterface() but in Context Class.");
+
+            //string name = "SorinBurtoiu";
+            //int count = 0;
+
+            //foreach (char c in name)
+            //{
+            //    count += (int)c;
+            //}
+
+            //Console.WriteLine("Suma literelor este : " + count.ToString());
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class Context
+    {
+        private Strategy _strategy;
+        public Context()
+        {
+        }
+
+        // Constructor
+        public Context(Strategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            _strategy.AlgorithmInterface();
+        }
+    }
+}
